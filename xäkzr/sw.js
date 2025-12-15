2b5ba64b865238012a3feb680dc74b5311935104
ciradhivat <57982312+ciradhivat@users.noreply.github.com>
2025-12-15 10:13:23 +0700

    dpqtvnxäkzr

---
+++ xäkzr/sw.js
@@ -0,0 +1,18 @@
+﻿const CACHE_NAME = 'pali-game-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
