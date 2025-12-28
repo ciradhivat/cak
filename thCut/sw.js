550f4bc94bd4297461b2828eb4a424af105ffd12
ciradhivat <57982312+ciradhivat@users.noreply.github.com>
2025-12-28 20:59:52 +0700

    Add files via upload

---
+++ thCut/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'thai-tokenizer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
